a8e81b5473e501c1a5cb7865a3e4e752c6299bd9
Sayantan Pal <sayantanpal100@Sayantans-MacBook-Pro.local>
2025-02-23 13:35:33 +0530

    _2_Need_of_file_handling written on 22.2.25

---
+++ _2_Need_of_file_handling.py
@@ -0,0 +1,18 @@
+'''Here the data is stored in the form of variables in ram so after switching 
+off the computer the data will be deleted. But in real life, data is stored in files.'''
+
+
+# age = int(input("Enter your age: "))
+# if age < 18:
+#     print("You are not allowed to watch this movie")
+# else:
+#     print("You can watch this movie")
+
+
+'''Thus to store the data in files, we need to use .txt file in python'''
+
+
+# age = (input('Enter your age: '))
+# age_file =open("age.txt", "w")
+# age_file.write(age)
+# age_file.close()
